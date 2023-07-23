282b7151c3457f468dc41ea562c7cabaedc4d531
Shuyuan Yang <1257173962@qq.com>
2023-07-23 18:37:00 -0400

    sync main

--- click.py
+++ click.py
@@ -1,7 +1,9 @@
+import sys
 import os
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 from tkinter import Toplevel as tkToplevel
+from PIL import Image, ImageTk
 import hashlib
 import datetime
 import time
@@ -17,14 +19,14 @@ class LoginDialog(tk.Toplevel):
         self.title("Click Relief")
         self.geometry("500x300")
 
-        self.label = tk.Label(self, text="Enter password:")
+        self.label = tk.Label(self, text="输入密码:")
         self.label.pack(pady=5)
 
         self.password_entry = tk.Entry(self, show="*")
         self.password_entry.pack(pady=5)
 
         self.submit_button = tk.Button(
-            self, text="Submit", command=self.check_password)
+            self, text="提交", command=self.check_password)
         self.submit_button.pack(pady=5)
 
         self.protocol('WM_DELETE_WINDOW', self.on_closing)
@@ -35,7 +37,7 @@ class LoginDialog(tk.Toplevel):
         if entered_password == self.master.password:
             self.destroy()
         else:
-            messagebox.showerror("Error", "Incorrect password.")
+            messagebox.showerror("Error", "密码错误")
 
     def on_closing(self):
         self.master.destroy()  # End the main program
@@ -53,12 +55,11 @@ class Application(tk.Tk):
         about_menu = tk.Menu(menu)
         menu.add_cascade(label="帮助", menu=about_menu)
         about_menu.add_command(label="关于", command=self.show_about)
-        self.createcommand('::tk::mac::ShowAbout', self.show_about)
 
         self.password = self.create_password()
         self.login()
 
-        self.label_iter = tk.Label(self, text="Number of clicks:")
+        self.label_iter = tk.Label(self, text="点击次数:")
         self.label_iter.grid(row=0, column=0)
 
         self.iter_var = tk.StringVar()
@@ -66,10 +67,10 @@ class Application(tk.Tk):
         self.entry_iter.grid(row=0, column=1)
 
         self.file_button = tk.Button(
-            self, text="Choose File", command=self.select_file)
+            self, text="选择文件", command=self.select_file)
         self.file_button.grid(row=1, column=0)
 
-        self.start_button = tk.Button(self, text="Start", command=self.start)
+        self.start_button = tk.Button(self, text="开始", command=self.start)
         self.start_button.grid(row=1, column=1)
 
         self.position_label = tk.Label(self, text="")
@@ -98,7 +99,7 @@ class Application(tk.Tk):
         if filename:
             self.filename = filename
             messagebox.showinfo(
-                "Info", f"Selected file: {os.path.basename(self.filename)}")
+                "Info", f"使用文件: {os.path.basename(self.filename)}")
 
     def login(self):
         login_dialog = LoginDialog(self)
@@ -112,19 +113,19 @@ class Application(tk.Tk):
 
     def start(self):
         if not self.filename:
-            messagebox.showerror("Error", "No file selected.")
+            messagebox.showerror("Error", "缺少文本文件")
             return
 
         self.start_button["state"] = "disabled"
         messagebox.showinfo(
-            "Info", "Move the cursor to the desired position and press 's'.")
+            "Info", "移动鼠标到目标位置然后按's'来确认")
         self.start_button.focus_set()  # set focus on start button
 
         self.bind('<s>', self.save_position)
 
     def save_position(self, event):
         self.position = pyautogui.position()
-        self.position_label["text"] = f"Saved position: {self.position}"
+        self.position_label["text"] = f"点击位置: {self.position}"
         self.unbind('<s>')
 
         self.start_process()
@@ -135,7 +136,7 @@ class Application(tk.Tk):
 
     def process(self):
         if self.filename is None:
-            messagebox.showerror("Error", "No file selected.")
+            messagebox.showerror("Error", "缺少文本文件")
             return
 
         with open(self.filename, 'r', encoding='utf-8') as f:  # 读取用户选定的文件
@@ -158,16 +159,31 @@ class Application(tk.Tk):
             elapsed_time = time.time() - start_time
             remaining_time = elapsed_time * (num_iter - (i+1)) / (i+1)
             eta = datetime.timedelta(seconds=int(remaining_time))
-            self.progress_label['text'] = f"Progress: {i+1}/{num_iter}, ETA: {eta}"
+            self.progress_label['text'] = f"进度: {i+1}/{num_iter}, 剩余时间: {eta}"
             self.update_idletasks()  # update GUI
 
             time.sleep(5)
 
         self.start_button["state"] = "normal"
-        messagebox.showinfo("Info", "Process completed!")
+        messagebox.showinfo("Info", "任务完成!")
 
     def show_about(self):
-        tk.messagebox.showinfo("关于", "版本: 1.0\n作者: 脑子露馅")
+        about_window = tk.Toplevel(self)
+        about_window.title("关于")
+        if getattr(sys, 'frozen', False):
+            application_path = sys._MEIPASS
+        else:
+            application_path = os.path.dirname(os.path.abspath(__file__))
+
+        image_path = os.path.join(application_path, 'about.png')
+        image = Image.open(image_path)
+        image = image.resize((500, 500), Image.LANCZOS)
+        photo = ImageTk.PhotoImage(image)
+        about_window.geometry(f"{image.width}x{image.height}")
+        about_window.resizable(False, False)
+        label = tk.Label(about_window, image=photo)
+        label.image = photo
+        label.pack()
 
 
 if __name__ == "__main__":
